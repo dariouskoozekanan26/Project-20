06027a6e7b9df7c41065d01d392de5a9037a347c
dariouskoozekanan26 <73252547+dariouskoozekanan26@users.noreply.github.com>
2020-11-19 19:42:56 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var movingRect,fixedRect;
+
+function setup() {
+  createCanvas(800,800);
+  fixedRect= createSprite(200,500,80,50);
+  fixedRect.debug=true;
+  fixedRect.shapeColor="green";
+  movingRect=createSprite(400, 200, 60, 50);
+  movingRect.debug=true;
+  movingRect.shapeColor="green";
+}
+
+function draw() {
+  background(0);  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+  console.log(movingRect.x-fixedRect.x);
+  if(movingRect.x-fixedRect.x<movingRect.width/2 + fixedRect.width/2 &&
+    fixedRect.x-movingRect.x<movingRect.width/2 + fixedRect.width/2 &&
+    movingRect.y-fixedRect.y<movingRect.height/2 + fixedRect.height/2 &&
+    fixedRect.y-movingRect.y<movingRect.height/2 + fixedRect.height/2){
+   movingRect.shapeColor="red";
+   fixedRect.shapeColor="red";
+  }
+  else{
+    movingRect.shapeColor="green";
+   fixedRect.shapeColor="green";
+  }
+  drawSprites();
+} 
